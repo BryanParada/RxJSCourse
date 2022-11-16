5f828e4a8fccc8db9df2e551b6e658f678a0113e
Bryan Parada <bryan.parada.cea@gmail.com>
2022-11-16 20:13:16 -0300

    R34 - map

--- src/app/components/operators/map-pluck/map-pluck.component.ts
+++ src/app/components/operators/map-pluck/map-pluck.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { fromEvent, range } from "rxjs";
+import { map } from "rxjs/operators";
 
 @Component({
   selector: 'app-map-pluck',
@@ -17,9 +19,43 @@ export class MapPluckComponent implements OnInit {
 
    initComponent(){
 
+      //  range(1,5).pipe(
+      //     map<number,number>( val => {
+      //       return val * 10 
+      //     }
+      //     )
+      //  )
+      //  .subscribe(console.log )
+
+  //   range(1,5).pipe(
+  //     map<number,number>( val => val * 10 )
+  //  )
+  //  .subscribe(console.log )
+
+//    range(1,5).pipe(
+//     map<number,string>( val => (val * 10).toString() )
+//  )
+//  .subscribe(console.log )
+
+    const keyup$ = fromEvent<KeyboardEvent>( document, 'keyup');
+
+    const keyupCode$ = keyup$.pipe(
+      map( event => event.code)
+    )
+
+    keyupCode$.subscribe( code => console.log('map ', code)  );
+
+    
 
 
 
-   }
+
+
+
+
+
+
+
+   } 
 
 }
