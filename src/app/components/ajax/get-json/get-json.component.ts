f324564c66ee61c6e553aaa7634624fa779b1c0e
Bryan Parada <bryan.parada.cea@gmail.com>
2022-12-06 13:30:05 -0300

    R70 - getJson & ajax difference

--- src/app/components/ajax/get-json/get-json.component.ts
+++ src/app/components/ajax/get-json/get-json.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { ajax} from 'rxjs/ajax';
+import { catchError, of } from 'rxjs';
+import { ajax, AjaxError } from 'rxjs/ajax';
 
 @Component({
   selector: 'app-get-json',
@@ -21,17 +22,46 @@ export class GetJsonComponent implements OnInit {
 
    initComponent(){
 
-    // const url = 'https://httpbin.org/delay/1'
-    const url = 'https://api.github.com/users?per_page=5'
+    const url = 'https://httpbinXXX.org/delay/1'
+    //const url = 'https://api.github.com/users?per_page=5'
 
-    const obs$ = ajax.getJSON(  url, {
-      //headers
-      'Content-Type': 'application/json',
-      'my-super-token': 'ABC123'
-    } );
+    const handleError = (resp: AjaxError) => {
+      console.warn('error', resp.message);
+      return of({
+        ok:false,
+        users: []
+      })
+    }
 
-    obs$.subscribe(data => console.log('data:', data));
+    // const obs$ = ajax.getJSON(  url, {
+    //   //headers
+    //   'Content-Type': 'application/json',
+    //   'my-super-token': 'ABC123'
+    // } );
 
+    // const obs$  = ajax.getJSON( url ).pipe(
+    //   catchError(handleError)
+    // );
+    // const obs2$ = ajax( url ).pipe(
+    //   catchError(handleError)
+    // );
+
+    const obs$  = ajax.getJSON( url );
+    const obs2$ = ajax( url );
+    
+    //obs$.subscribe(data => console.log('getJson:', data));
+    //obs2$.subscribe(data => console.log('ajax:', data));
+    
+    obs$.pipe(
+      catchError(handleError)
+    )
+    .subscribe({
+      next: val => console.log('next', val),
+      error: err => console.log('error en subs:', err),
+      complete: () => console.log('complete'),
+           
+      
+    });
     
 
 
